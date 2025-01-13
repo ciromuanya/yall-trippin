ed3a6f469e7d3fcb47192807d041f6e137be434a
Almaze Lema <chupzilemma12@gmail.com>
2025-01-13 03:12:07 -0500

    created Header component for username and profile logo. exported for use in App.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className="logo">Y'all Trippin'</div>
+            <div className="profile-icon">CI</div>
+        </div>
+    );
+}
+
+export default Header;
