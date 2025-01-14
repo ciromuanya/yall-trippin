a02f67f2441afeb71c9249b8ed864c48e5dac140
Almaze Lema <chupzilemma12@gmail.com>
2025-01-14 16:25:15 -0500

    changed app.js to handle intreoduction of routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Welcome from './components/Welcome';
-import Gallery from './components/Gallery';
-import PlanTrip from './components/PlanTrip';
-import PopularDestinations from './components/PopularDestinations';
-import BottomNav from './components/BottomNav';
+import "./App.css";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import Gallery from "./components/Gallery";
+import PlanTrip from "./components/PlanTrip";
+import PopularDestinations from "./components/PopularDestinations";
+import BottomNav from "./components/BottomNav";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Welcome />
-      <Gallery />
-      <PlanTrip />
-      <PopularDestinations />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/plan-trip" element={<PlanTrip />} />
+        <Route path="/popular-destinations" element={<PopularDestinations />} />
+      </Routes>
       <BottomNav />
-    </div>
+    </Router>
   );
 }
 
