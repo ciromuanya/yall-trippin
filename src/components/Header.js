b63ca68913cd60147ccbda36ffa400b28fb38037
Almaze Lema <chupzilemma12@gmail.com>
2025-01-14 16:24:09 -0500

    changes in look

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <div className='header'>
-            <div className="logo">Y'all Trippin'</div>
-            <div className="profile-icon">CI</div>
-        </div>
-    );
+  return (
+    <div className="header">
+      <div className="logo">Y'all Trippin'</div>
+      <div className="profile-icon">CI</div>
+    </div>
+  );
 }
 
 export default Header;
